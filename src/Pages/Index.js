a316be77318abcabdbe742ad0174373312685888
Alec Hannaford <alec322@gmail.com>
2023-03-12 08:46:25 -0700

    year dropdown

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,11 +1,11 @@
-import {useLoaderData} from 'react-router-dom'
+import {useLoaderData, Form} from 'react-router-dom'
 
 
 function Index(props){
     const data = useLoaderData()
     const months = []
     //grab month string from parsed date str
-    data.forEach((element) => months.push(new Intl.DateTimeFormat("en-US", { month: "long" }).format(new Date(element.date))))
+    data.forEach((element) => months.push(new Intl.DateTimeFormat("en-US", { year : "numeric" }).format(new Date(element.date))))
     console.log(months)
 
     //grab only unique months
@@ -15,7 +15,32 @@ function Index(props){
     const uniqueMonths = months.filter(isUnique)
 
     console.log(uniqueMonths)
-    return <h1>Index COMPONENT</h1>
+    return (<>
+        <h1>Index COMPONENT</h1>
+        <Form>
+            <input placeholder='year'/>
+            <input placeholder='month'/>
+            <input type = 'submit'/>
+        </Form>
+        <div className="dropdown">
+            <button className="dropbtn">Select Year</button>
+            <div className = "dropdown-content">
+                {uniqueMonths.map((element, index) => {
+                    return <div key = {index}>{element}</div>
+                })}
+            </div>
+        </div>
+        <div className="dropdown">
+            <button className="dropbtn">Select Month</button>
+            <div className = "dropdown-content">
+                {uniqueMonths.map((element, index) => {
+                    return <div key = {index}>{element}</div>
+                })}
+            </div>
+        </div>
+
+    </>)
+
 }
 
 export default Index
