4c95394d40a2be458386fa3fdbf7f282342f2e69
Alec Hannaford <alec322@gmail.com>
2023-03-17 15:56:14 -0700

    day filter page

--- src/Pages/ShowDays.js
+++ src/Pages/ShowDays.js
@@ -1,9 +1,22 @@
 import { useLoaderData } from "react-router-dom"
 
 function ShowDays(props){
-    const data = useLoaderData()
-    console.log(data)
-    return <h1>ShowDays COMPONENT</h1>
+    const uniqueDays = useLoaderData()
+    return (
+        <>
+        <h1></h1>
+        <div className="dropdown">
+            <button className="dropbtn">Select Day</button>
+            <div className = "dropdown-content">
+                {uniqueDays.map((element, index) => {
+                    return <a 
+                    key = {index}>{element}</a>
+                })}
+            </div>
+        </div>
+        </>
+
+    )
 }
 
 export default ShowDays
