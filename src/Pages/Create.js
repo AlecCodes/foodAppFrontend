2db9dcc59cf6fd6ecb675cf6369141044f15fbbf
Alec Hannaford <alec322@gmail.com>
2023-03-23 19:04:12 -0700

    working forms

--- src/Pages/Create.js
+++ src/Pages/Create.js
@@ -1,14 +1,16 @@
 import {Form} from 'react-router-dom'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {searchResult} from '../Nutritionix/searchResult'
 import {nutritionGetter} from '../Nutritionix/nutritionGetter'
+
 
 function Create(props){
     const [nameState, setNameState] = useState('')
     const [carbState, setCarbState] = useState(0)
     const [dateState, setDateState] = useState({myDateField: new Date().toISOString().split('T')[0]})
+    const [qtyState, setQtyState] = useState(0)
     const [resultsState, setResultsState] = useState('')
-
+    const [ratioState, setRatioState] = useState(null)
 
 /////////////////////////////////////////////
 // Change Handlers
@@ -17,6 +19,7 @@ function Create(props){
         setNameState(event.target.value)
         //confirm we're not breaking search/instant endpoint w null value
         if (event.target.value.length >= 1){
+            //async to wait for api
             const data = await searchResult(event)
             setResultsState(data.common.concat(data.branded))
         }else{
@@ -29,16 +32,41 @@ function Create(props){
     function dateChangeHandler(event){
         setDateState(event.target.value)
     }
+    function qtyChangeHandler(event){
+        if (event.target.value > 0){
+            setQtyState(event.target.value)
+        } else {
+            setQtyState(0)
+        }
+    }
 /////////////////////////////////////////////
 // Click Handlers
 /////////////////////////////////////////////
-    function handleSuggestionClick(foodname){
-        setNameState(foodname)
-        nutritionGetter(foodname).then(json => setCarbState(json.foods[0].nf_total_carbohydrate))
+
+    //Change to async function??? Then call setCarbState once promise fulfilled?
+    async function handleSuggestionClick(suggestion){
+        setNameState(suggestion.food_name)
+        setQtyState(suggestion.serving_qty)
+        nutritionGetter(suggestion.food_name).then(json => setCarbState(json.foods[0].nf_total_carbohydrate))
         setResultsState([])
     }
-    
-    return (
+
+/////////////////////////////////////////////
+// UseEffect hooks
+/////////////////////////////////////////////
+    useEffect(() => {
+        if (qtyState !== 0){
+            const newRatio = carbState/qtyState
+            setRatioState(newRatio)
+        }
+    }, [carbState, qtyState])
+// eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => {
+        const netCarbs = ratioState*qtyState
+        setCarbState(netCarbs)
+    }, [qtyState])
+
+return (
     <>
         <h1>Create COMPONENT</h1>
         <Form autoComplete='off' action="/create" method ="post">
@@ -52,7 +80,7 @@ function Create(props){
                         {resultsState.map((suggestion) => {
                             return<li
                             key={suggestion.food_name}
-                            onClick={() => handleSuggestionClick(suggestion.food_name)}
+                            onClick={() => handleSuggestionClick(suggestion)}
                             >
                             <span className='foodname'>{suggestion.food_name}</span>
                             <span className='foodqty'>{suggestion.serving_qty} {suggestion.serving_unit}</span>
@@ -61,12 +89,17 @@ function Create(props){
                     </ul>
                 )}
             </div>
+            
+            <input type = 'number' placeholder='quantity'
+            onChange={qtyChangeHandler}
+            value={qtyState}
+            />
 
             <input type = 'number' name = 'carbs' placeholder='carbs'
             onChange={carbChangeHandler}
             value={carbState}
             />
-            
+
             <input type = 'date' name = 'date' placeholder='date'
             value={dateState.myDateField}
             onChange={dateChangeHandler}
