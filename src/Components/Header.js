166edbfde24343c1c188f692e5651af829d612b3
Alec Hannaford <alec322@gmail.com>
2023-03-25 16:52:31 -0700

    nav class bar with tab view in header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,52 @@
 import {Link} from 'react-router-dom'
+import {useState} from 'react'
+
 
 function Header(props){
-    return <nav>
-        <div className='navbar container'>
-            <div className='row'></div>
-            <div className='row'>
-                <Link
-                className='col'
-                to='showyears'>Show</Link>
-                <Link 
-                className='col'
-                to='create'>Create New</Link>
-            </div>
-            <div className='row'></div>
-        </div>
-    </nav>
+    // <nav className='navbar'>
+    //     <div className='container nav nav-tabs'>
+    //         <div className='row'></div>
+    //         <div className='row'>
+    //             <div className='col nav-item'>
+    //             <Link
+    //             className='nav-link active'
+    //             to='showyears'>Show</Link>
+    //             </div>
+
+    //             <div className='col'></div>
+    //             <div className='col'></div>
+    //             <div className='col'></div>
+    //             <div className='col'></div>
+    //             <div className='col'></div>
+    //             <div className='col'></div>
+    //             <div className='col'></div>
+    //             <div className='col'></div>
+
+
+    //             <Link 
+    //             className='col'
+    //             to='create'>Create New</Link>
+    //         </div>
+    //         <div className='row'></div>
+    //     </div>
+    // </nav>
+
+    const [showTabState, setShowTabState] = useState('')
+
+    return (
+    <ul className='nav nav-tabs'>
+        <li>
+            <Link to='showyears'
+            className={`nav-link`}
+            > Show </Link>
+        </li>
+        <li>
+            <Link to='create'
+            className={`nav-link`}
+            > Create </Link>
+        </li>
+    </ul>
+    )
 }
 
 export default Header;
