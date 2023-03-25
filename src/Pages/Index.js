c14df5d4c86bb7d9bf05872cf80dc7a3466fc90c
Alec Hannaford <alec322@gmail.com>
2023-03-25 11:42:28 -0700

    ../ link for scss

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -3,7 +3,9 @@ import {uniqueMonthsFilter, uniqueYearsFilter} from '../filters'
 
 function Index(props){
     const data = useLoaderData()
-    
+    return(
+        <h1 className='LANDING'>OK YO</h1>
+    )
 }
 
 export default Index
