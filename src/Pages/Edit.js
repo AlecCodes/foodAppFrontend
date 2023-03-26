474a56626b8e2dc30a7a9071c9c999b3a311490b
Alec Hannaford <alec322@gmail.com>
2023-03-25 18:14:23 -0700

    edit page styled

--- src/Pages/Edit.js
+++ src/Pages/Edit.js
@@ -7,11 +7,28 @@ function Edit(props){
         <h2
         className="p-4"
         >Set the record straight</h2>
-        <Form action = {`/edit/${data.id}`} method = "post">
-            <input type="text" name="name" defaultValue={data.name}></input>
-            <input type="date" name="date" defaultValue={data.date}></input>
-            <input type="number" name='carbs' defaultValue={data.carbs}></input>
-            <input type='submit'/>
+        <Form
+        className="container" 
+        action = {`/edit/${data.id}`} method = "post">
+            <div className="form-group">
+                <label>Food Name</label>
+                <input
+                className="form-control" 
+                type="text" name="name" defaultValue={data.name}></input>
+            </div>
+            <div className="form-group">
+                <label>Carbs</label>
+                <input 
+                className="form-control"
+                type="number" name='carbs' defaultValue={data.carbs}></input>
+            </div>
+            <div className="form-group">
+                <label>Date</label>
+                <input 
+                className="form-control"
+                type="date" name="date" defaultValue={data.date}></input>
+            </div>
+            <input className="btn btn-primary m-4" type='submit'/>
         </Form>
         </>
     )
